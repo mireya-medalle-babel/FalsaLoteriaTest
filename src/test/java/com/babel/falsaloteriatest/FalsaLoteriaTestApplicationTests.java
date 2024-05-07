f50f7f00f0e880ce15d548d04902d245ff20553a
Mireya Medalle Merino <mireya.medalle@babelgroup.com>
2024-05-07 13:36:42 +0200

    Init

---
+++ src/test/java/com/babel/falsaloteriatest/FalsaLoteriaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babel.falsaloteriatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalsaLoteriaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
